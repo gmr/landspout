a4cb0df4e5dd5dca67354629a9b004f1809bebae
Gavin M. Roy <gavinr@aweber.com>
2018-09-10 13:40:18 -0400

    Add Python 3.7, use travis to deploy

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: BSD License'
     ],
     keywords='static website generation',
